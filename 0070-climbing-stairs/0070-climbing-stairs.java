8258a8c6ab7f62dbd72f7a09c45c24bce613de6b
MohammadM01 <141570969+MohammadM01@users.noreply.github.com>
2026-09-14 22:59:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (72.83%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,15 +2,15 @@ class Solution {
     public int climbStairs(int n) {
         return fun(0, n);
     }
+
     int fun(int i, int n) {
-        if (i == n || n==1)
+        if (i == n || n == 1)
             return 1;
         if (i > n)
             return 0;
         int current = 2;
         int previous = 1;
-    
-        for(i=3;i<=n;i++){
+        for (i = 3; i <= n; i++) {
             int ans = current + previous;
             previous = current;
             current = ans;
